b79d05c604cac5c9d55bd4d3a5a71060a2ac047b
Pau1-McK <paulmckeon007@gmail.com>
2023-11-06 21:33:41 +0000

    v1

--- DATA FORMATS/SSH keys.py
+++ DATA FORMATS/SSH keys.py
@@ -1,19 +1,23 @@
-# The SSH public key
-ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCtPLqba+GFvDHdFVs1Vvdk56cKqqw5cdomlu034666UsoFIqkig8H5kNsNefSpaR/iU7G0ZKCiWRRuAbTsuHN+Cz526XhQvzgKTBkTGYXdF/WdG/6/umou3Z0+wJvTZgvEmeEclvitBrPZkzhAK1M5ypgNR4p8scJplTgSSb84Ckqul/Dj/Sh+fwo6sU3S3j92qc27BVGChpQiGwjjut4CkHauzQA/gKCBIiLyzoFcLEHhjOBOEErnvrRPWCIAJhALkwV2rUbD4g1IWa7QI2q3nB0nlnjPnjjwaR7TpH4gy2NSIYNDdC1PZ8reBaFnGTXgzhQ2t0ROBNb+ZDgH8Fy+KTG+gEakpu20bRqB86NN6frDLOkZ9x3w32tJtqqrJTALy4Oi3MW0XPO61UBT133VNqAbNYGE2gx+mXBVOezbsY46C/V2fmxBJJKY/SFNs8wOVOHKwqRH0GI5VsG1YZClX3fqk8GDJYREaoyoL3HKQt1Ue/ZW7TlPRYzAoIB62C0= bschneier@facts"
+# First i had to convert the .pub to a .pem with ssh-keygen and it gave me the key bellow 
 
-# Split the SSH public key string and extract the base64-encoded key
-key_parts = ssh_public_key.split()
-encoded_key = key_parts[1]
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 
-# Decode the base64-encoded key
-decoded_key = base64.b64decode(encoded_key)
+public_key_pem = """
+-----BEGIN RSA PUBLIC KEY-----
+MIIBigKCAYEArTy6m2vhhbwx3RVbNVb3ZOenCqqsOXHaJpbtN+OuulLKBSKpIoPB
++ZDbDXn0qWkf4lOxtGSgolkUbgG07Lhzfgs+dul4UL84CkwZExmF3Rf1nRv+v7pq
+Lt2dPsCb02YLxJnhHJb4rQaz2ZM4QCtTOcqYDUeKfLHCaZU4Ekm/OApKrpfw4/0o
+fn8KOrFN0t4/dqnNuwVRgoaUIhsI47reApB2rs0AP4CggSIi8s6BXCxB4YzgThBK
+5760T1giACYQC5MFdq1Gw+INSFmu0CNqt5wdJ5Z4z5448Gke06R+IMtjUiGDQ3Qt
+T2fK3gWhZxk14M4UNrdETgTW/mQ4B/BcvikxvoBGpKbttG0agfOjTen6wyzpGfcd
+8N9rSbaqqyUwC8uDotzFtFzzutVAU9d91TagGzWBhNoMfplwVTns27GOOgv1dn5s
+QSSSmP0hTbPMDlThysKkR9BiOVbBtWGQpV936pPBgyWERGqMqC9xykLdVHv2Vu05
+T0WMwKCAetgtAgMBAAE=
+-----END RSA PUBLIC KEY-----
+"""
 
-# Extract the modulus (n) from the decoded key
-modulus_start = decoded_key.find(b'\x00\x00\x00\x80')  # The modulus usually starts with 0x00 0x00 0x00 0x80
-modulus_end = decoded_key.find(b'\x00\x00\x01\x00', modulus_start)  # The modulus usually ends with 0x00 0x00 0x01 0x00
+public_key = serialization.load_pem_public_key(public_key_pem.encode())
 
-modulus_bytes = decoded_key[modulus_start + 4 : modulus_end]  # Extract the modulus bytes
-modulus_decimal = int.from_bytes(modulus_bytes, byteorder='big')  # Convert to a decimal integer
-
-# Print the modulus as a decimal integer
-print(modulus_decimal)
+modulus = public_key.public_numbers().n
+print(modulus)
