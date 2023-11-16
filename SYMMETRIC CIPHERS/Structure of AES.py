ee728424914f6835bae3b995c898c30434adda8a
Pau1-McK <paulmckeon007@gmail.com>
2023-11-16 11:00:19 +0000

    v1

--- SYMMETRIC CIPHERS/Structure of AES.py
+++ SYMMETRIC CIPHERS/Structure of AES.py
@@ -1,9 +1,9 @@
 def bytes2matrix(text):
-    """ Converts a 16-byte array into a 4x4 matrix.  """
+
     return [list(text[i:i+4]) for i in range(0, len(text), 4)]
 
 def matrix2bytes(matrix):
-    """ Converts a 4x4 matrix into a 16-byte array.  """
+
     return bytes([matrix[i][j] for i in range(4) for j in range(4)])
 
 matrix = [
